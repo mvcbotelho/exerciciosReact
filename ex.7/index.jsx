2405af0cf6c9d3e110d7c1d4a2b971e0f4fbd12f
mvcbotelho <marcus.itec@gmail.com>
2018-02-16 15:30:13 -0300

    Exercicios 7 e 8 resolvidos

--- ex.7/index.jsx
+++ ex.7/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Field from './field'
+import ClassComponent from './classComponent'
 
 ReactDOM.render(
-    <Field initalValue="Teste" />
+    <ClassComponent value='Componente de Classe' /> 
     ,document.getElementById('app')
 )
