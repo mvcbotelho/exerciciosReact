245629e887d70c40d81f59cbcf8c2a443cc5824f
mvcbotelho <marcus.itec@gmail.com>
2018-02-21 09:03:30 -0300

    Criação da action e do reducer para monitorar o estado do campo

--- ex/field.js
+++ ex/field.js
@@ -1,5 +1,7 @@
 import React, { Components, Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeValue } from './fieldActions'
 
 class Field extends Component{
 
@@ -8,7 +10,7 @@ class Field extends Component{
       <div>
         <label>{ this.props.value }</label>
         <br/>
-        <input onChange={ this.handleChange} value={ this.props.value } />
+        <input onChange={this.props.changeValue} value={ this.props.value } />
       </div>
     )
   }
@@ -20,4 +22,8 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(Field)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ changeValue }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Field)
